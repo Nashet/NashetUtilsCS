404309e2e690e4331fef1187167ee2a634bef459
Nashet <Nashet@users.noreply.github.com>
2021-11-18 17:03:34 +0300

    Improve restarter

--- Assets/NashUtilsCs/RestartHelper/RestartHelper.cs
+++ Assets/NashUtilsCs/RestartHelper/RestartHelper.cs
@@ -1,4 +1,6 @@
 ﻿#if UNITY_EDITOR
+using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using UnityEditor;
 using UnityEngine;
@@ -7,21 +9,32 @@ namespace NashUtilsCs.RestartHelper
 {
     public static class RestartHelper
     {
+        private static bool _isRestarting;
+        private static readonly System.Diagnostics.Stopwatch _timer = new System.Diagnostics.Stopwatch();
+
         // %(ctrl on Windows, cmd on macOS), # (shift), & (alt)
         [MenuItem("EDITORS/M3/RestartHelper &r")]
         public static void RestartIfCompilingIsDone()
         {
-            Log("Registered attempt to restart editor Ж)");
-            UnityEditor.EditorApplication.isPlaying = false;
-            RestartAsync();
+            if (_isRestarting)
+            {
+                Log($"All ready restarting. But you can push  alt a");
+            }
+            else
+            {
+                ClearConsole();
+                Log("Registered attempt to restart editor Ж)");
+                RestartAsync();    
+            }
         }
-        
+
         // %(ctrl on Windows, cmd on macOS), # (shift), & (alt)
         [MenuItem("EDITORS/M3/AbortLaunch &a")]
         public static void AbortLaunch()
         {
             Log("Registered attempt to abort launch");
             UnityEditor.EditorApplication.isPlaying = false;
+            _isRestarting = false;
         }
 
         private static async void AbortLaunchAsync()
@@ -29,33 +42,50 @@ namespace NashUtilsCs.RestartHelper
             while (UnityEditor.EditorApplication.isPlaying)
             {
                 Log("Waiting to stop");
-                await Task.Delay(1000);
+                await Task.Delay(100);
                 UnityEditor.EditorApplication.isPlaying = false;
             }
         }
 
         private static void Log(string text)
         {
-            Debug.Log("[RestartHelper] "+ text);
+            Debug.LogError("[RestartHelper] " + text);
         }
 
-        private static async void RestartAsync ()
+        private static async void RestartAsync()
         {
+            _isRestarting = true;
+            _timer.Start();
+            UnityEditor.EditorApplication.isPlaying = false;
+            await Task.Delay(40);
             while (UnityEditor.EditorApplication.isPlaying)
             {
                 Log("Waiting to stop");
-                await Task.Delay(1000);
+                await Task.Delay(100);
             }
-            
+
+            var stopingTime = _timer.ElapsedMilliseconds;
+            Log($"Stopping took {stopingTime} ms.");
             while (UnityEditor.EditorApplication.isCompiling)
             {
-                Log("Waiting for compiling");
-                await Task.Delay(1000);
+                Log($"Waiting for compiling");
+                await Task.Delay(100);
             }
-            
-            Log("Editor is ready to go");
+
+            Log($"Editor is ready to go. Compiling took {_timer.ElapsedMilliseconds - stopingTime} ms");
             UnityEditor.EditorApplication.EnterPlaymode();
-            Log("Editor is loading :)");
+
+            _timer.Stop();
+            Log($"Editor is loading :) totaly took {_timer.ElapsedMilliseconds} ms");
+            _isRestarting = false;
+        }
+
+        private static void ClearConsole()
+        {
+            var assembly = Assembly.GetAssembly(typeof(SceneView));
+            var type = assembly.GetType("UnityEditor.LogEntries");
+            var method = type.GetMethod("Clear");
+            method.Invoke(new object(), null);
         }
     }
 }
